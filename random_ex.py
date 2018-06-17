ab5363d63955215f1ce31b9178d3351274de7e64
rabiaasif <rabiaasif@Rabias-MacBook-Pro-2.local>
2018-06-17 00:18:46 -0400

    adding random exercises that were given to me

--- random_ex.py
+++ random_ex.py
@@ -64,8 +64,63 @@ def factorial(n):
         return 1
     else:
         return n * factorial(n-1)
+def next_num(n):
+    '''
+    exercise I was given, given an input like  n = [9, 2, 1] (number is 129) return the next number
+    '''
+   
+    n.reverse()
     
+    if n[-1] < 9:
+        n[-1] = n[-1]+1 
     
+        print n
+    else:
+        index = len(n)-1
+        while index != 0:
+            if n[index] == 9 :
+                n[index] = 0
+                if n[index-1] != 9:
+                    n[index-1] += 1
+            index = index -1 
+          
+        print n 
+        
+def add_array(array, other):
+    '''
+    given array and other ex [1,1,1] and [1,2,3], add the numbers of each index togehter
+    '''
+    index = 0
+    if len(array) == len(other):
+        for i in array:
+            array[index] = array[index] + other[index]
+            print array[index]
+            index +=1 
+    else:
+        index =  abs(len(other) - len(array))
+        while index < len(array):
+            array[index] = array[index] + other[0]
+            print array[index]
+            index +=1     
+def multiple(array, other):
+   # array = [0,1] -> 10
+    #other = [0, 2] -> 20 10x 20 = 200 
+    array.reverse()
+    other.reverse()
+    new_a = '' 
+    new_o = ''
+    for i in array:
+        new_a += str(i)
+    for i in other:
+        new_o += str(i)
+    
+    for i in str(int(new_o)*int(new_a)):
+        print i
+    
+
+        
+        
+        
     
     
     
